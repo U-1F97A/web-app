986328ab1d9c351f9719973da43e489006b83ebf
Cilvia333 <cilvia333x@gmail.com>
2020-05-26 22:47:43 +0900

    add: button component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,33 +3,22 @@ import Head from 'next/head';
 import * as React from 'react';
 import styled from 'styled-components';
 
+import Button from '~/components/button';
+import Layout from '~/components/layout';
+
 const Home: NextPage = () => {
   return (
     <>
-      <Wrapper>
-        <Head>
-          <title>Create Next App</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-
-        <main></main>
-
-        <footer>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{' '}
-            <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-          </a>
-        </footer>
-      </Wrapper>
+      <Layout title="🥺">
+        <Main>
+          <Button text="submit" />
+        </Main>
+      </Layout>
     </>
   );
 };
 
-const Wrapper = styled.div`
+const Main = styled.main`
   min-height: 100vh;
   padding: 0 0.5rem;
   display: flex;
