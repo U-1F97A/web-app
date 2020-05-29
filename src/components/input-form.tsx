6a512b9b947290beff874ed51de2cf6929012e5e
kotomi1338 <kotomin1216@gmail.com>
2020-05-29 23:52:31 +0900

    fearture: 本のタイトルを送信して画面遷移する

--- src/components/input-form.tsx
+++ src/components/input-form.tsx
@@ -1,9 +1,11 @@
+import { useRouter } from 'next/router';
 import * as React from 'react';
 import { FC, useState } from 'react';
 
 import TextInput from './text-input';
 
 const InputForm: FC = () => {
+  const router = useRouter();
   const [state, setState] = useState({
     book_title: '',
   });
@@ -15,30 +17,24 @@ const InputForm: FC = () => {
     });
   };
 
-  const submitAlert = (event: React.MouseEvent) => {
-    event.persist();
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const error = Object.values(state).some((value) => {
-      return value.length === 0;
-    });
-
-    if (error) {
-      alert('未入力項目があります');
-    } else {
-      alert('送信します');
-    }
+    console.log(state.book_title);
+    router.push('/karte');
   };
 
   return (
     <>
       <h2>本のタイトルを入力してね</h2>
-      <TextInput
-        text="book_title"
-        value={state.book_title}
-        onChange={handleInputChange}
-      />
-      <p>{state.book_title}</p>
-      <button onClick={submitAlert}>送信</button>
+      <form onSubmit={handleSubmit}>
+        <TextInput
+          text="book_title"
+          value={state.book_title}
+          onChange={handleInputChange}
+        />
+        <p>{state.book_title}</p>
+        <input type="submit" value="送信" />
+      </form>
     </>
   );
 };
