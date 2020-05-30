2338d5dcf475d5215b62683d2bc0c36bce06b5bd
Cilvia333 <cilvia333x@gmail.com>
2020-05-31 04:57:03 +0900

    add: タイトルスタイル

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,33 @@
 import { NextPage } from 'next';
 import * as React from 'react';
+import styled, { css } from 'styled-components';
 
 import ButlerTalk from '~/components/butler-talk';
 import InterviewForm from '~/components/interview-form';
 import Layout from '~/components/layout';
+import { Fonts } from '~/styles/mixins';
+import { Colors } from '~/styles/variables';
 
 const Home: NextPage = () => {
   return (
     <>
       <Layout title="🥺">
-        <ButlerTalk>read, read, read!</ButlerTalk>
+        <ButlerTalk>
+          <Title>read, read, read!</Title>
+        </ButlerTalk>
         <InterviewForm />
       </Layout>
     </>
   );
 };
+
+const Title = styled.h1`
+  ${Fonts.h1}
+  color: ${Colors.black};
+
+  &::after{
+    
+  }
+`;
 
 export default Home;
