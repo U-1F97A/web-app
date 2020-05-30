cbceccc4bd0420e25c1a51ae01f0a0309c4ed74a
Cilvia333 <cilvia333x@gmail.com>
2020-05-31 06:17:21 +0900

    update: カルテ画面のスタイリング

--- src/pages/karte/index.tsx
+++ src/pages/karte/index.tsx
@@ -13,6 +13,8 @@ import { Colors } from '~/styles/variables';
 
 const Karte: NextPage = () => {
   const router = useRouter();
+  const [bookName, setBookName] = useState('');
+  const [inputText, setInputText] = useState('');
 
   return (
     <>
@@ -24,18 +26,10 @@ const Karte: NextPage = () => {
             </ButtlerText>
           </ButlerTalk>
         </ButlerWrapper>
-        <TextWrapper>
-          <WelcomeText>ようこそ！</WelcomeText>
-          <Description>
-            読書ができないなんて悩んでいませんか？
-            そんなあなたに、簡単な診断をするだけで、
-            おすすめの読書のスタイルを提案したします。
-          </Description>
-        </TextWrapper>
         <ButtonWrapper>
           <ConfirmButton
-            title={'さっそく、診断してみる'}
-            onClick={() => router.push('/karte')}
+            title={'送信'}
+            onClick={() => setBookName(inputText)}
           />
         </ButtonWrapper>
       </Layout>
@@ -44,19 +38,7 @@ const Karte: NextPage = () => {
 };
 const ButlerWrapper = styled.section`
   position: relative;
-  padding-bottom: 24px;
   margin-bottom: 48px;
-
-  &::after {
-    content: '';
-    position: absolute;
-    bottom: 0;
-    right: 0;
-    left: 0;
-    height: 3px;
-    width: 100%;
-    background-color: ${Colors.black};
-  }
 `;
 
 const TextWrapper = styled.section`
@@ -69,44 +51,6 @@ const ButtonWrapper = styled.section`
   position: relative;
   margin-bottom: 48px;
   text-align: center;
-`;
-
-const Title = styled.h1`
-  ${Fonts.h1}
-  color: ${Colors.black};
-  position: relative;
-  margin-bottom: 24px;
-  display: inline-block;
-
-  &::after{
-    content: '';
-    position: absolute;
-    bottom: -4px;
-    right: 0;
-    left: 0;
-    height: 12px;
-    width: 100%;
-    background-color: ${Colors.yellow};
-    z-index: -1;
-  }
-`;
-
-const SubTitle = styled.p`
-  ${Fonts.h4}
-  color: ${Colors.black};
-`;
-
-const WelcomeText = styled.h2`
-  ${Fonts.h2}
-  color: ${Colors.black};
-  margin-bottom: 24px;
-`;
-
-const Description = styled.p`
-  ${Fonts.text}
-  color: ${Colors.black};
-  max-width: 60%;
-  margin: auto;
 `;
 
 export default Karte;
